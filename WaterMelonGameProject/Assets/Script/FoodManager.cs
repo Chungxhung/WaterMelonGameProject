9796cadb64c6bcdb88d0d970390dd9bdeb3f773d
Minjeong Kang <132047865+Chungxhung@users.noreply.github.com>
2024-04-18 18:06:19 +0900

    script change

--- WaterMelonGameProject/Assets/Script/FoodManager.cs
+++ WaterMelonGameProject/Assets/Script/FoodManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FoodManager : MonoBehaviour
 {
+
+    public bool isClicked; // 버튼이 눌렸는지 안눌렸는지 여부
+
     // Start is called before the first frame update
     void Start()
     {
